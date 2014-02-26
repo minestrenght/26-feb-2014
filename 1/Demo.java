f4f185f16e274a1157e80777ac5b7c995775e8ce
minestrenght <minestrenght@gmail.com>
2014-02-26 22:29:17 +0530

    done

--- 1/Demo.java
+++ 1/Demo.java
@@ -2,5 +2,30 @@ import static java.lang.System.out;
 class Demo {
 
     public static void main(String[] args) {
+        Node start = new Node(null, 45);
+        Node cur = null;
+        cur = start;
+        while (null != cur) {
+            out.printf("info: %s\n", cur.info);
+            cur = cur.next;
+        }
+        out.println("=================");
+        start.next = new Node(null, 56);
+        cur = start;
+        while (null != cur) {
+            out.printf("info: %s\n", cur.info);
+            cur = cur.next;
+        }
+        out.println("=================");
+    }
+}
+
+class Node {
+    Node next;
+    int info;
+
+    Node(Node next, int info) {
+        this.next = next;
+        this.info = info;
     }
 }
